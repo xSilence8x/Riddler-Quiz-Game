8af09b8cd88309650dc9f029bdd0203dc12a4aa1
Marketa Dalecka <xsilence8x@hotmail.com>
2024-05-08 10:32:09 +0200

    pridano html na register success

--- riddler_project/riddler/riddler_game/urls.py
+++ riddler_project/riddler/riddler_game/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import HomeView, UserRegisterView, UserLoginView, KvizView, InvalidateTokenView, CustomPasswordResetView, CustomPasswordResetConfirmView, CustomPasswordResetDoneView, CustomPasswordResetCompleteView
+from .views import HomeView, UserRegisterView, redirect_view, UserLoginView, KvizView, InvalidateTokenView, CustomPasswordResetView, CustomPasswordResetConfirmView, CustomPasswordResetDoneView, CustomPasswordResetCompleteView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("login/", UserLoginView.as_view(), name="login"),
     path("register/", UserRegisterView.as_view(), name="register"),
+    path("registration-ok/", redirect_view, name="register-ok"),
     path("kviz/", KvizView.as_view(), name="kviz"),
     path('neplatny-token/', InvalidateTokenView.as_view(), name='invalid_token'),
     path('obnovit-heslo/', CustomPasswordResetView.as_view(), name='password_reset'),
